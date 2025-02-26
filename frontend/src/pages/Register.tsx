b62e05249ed9ae44babc6ef48a968e6a4f86c7f3
Ersin Isgor <ersinisgor@hotmail.com>
2025-02-26 22:14:53 +0300

    feat_fe: Add registration page with form for user account creation

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,48 @@
+const Register = () => {
+  return (
+    <form className="flex flex-col gap-5">
+      <h2 className="text-3xl font-bold">Create an Account</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          First Name
+          <input className="border rounded w-full py-1 px-2 font-normal"></input>
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Last Name
+          <input className="border rounded w-full py-1 px-2 font-normal"></input>
+        </label>
+      </div>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Email
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+        ></input>
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+        ></input>
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Confirm Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+        ></input>
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+        >
+          Create Account
+        </button>
+      </span>
+    </form>
+  );
+};
+
+export default Register;
