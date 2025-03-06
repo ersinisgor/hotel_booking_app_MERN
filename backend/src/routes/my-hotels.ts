31785c3fe75b4b1e74664a2473a882733e6c703e
Ersin Isgor <ersinisgor@hotmail.com>
2025-03-06 23:00:18 +0300

    feat_be: add multer for file uploads and create hotel model with image upload functionality, create my-hotels route

---
+++ backend/src/routes/my-hotels.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from "express";
+import multer from "multer";
+import cloudinary from "cloudinary";
+import Hotel, { HotelType } from "../models/hotels";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+router.post(
+  "/",
+  upload.array("imageFiles", 6),
+  async (req: Request, res: Response) => {
+    try {
+      const imageFiles = req.files as Express.Multer.File[];
+      const newHotel: HotelType = req.body;
+      const uploadPromises = imageFiles.map(async image => {
+        const b64 = Buffer.from(image.buffer).toString("base64");
+        let dataURI = "data:" + image.mimetype + ";base64," + b64;
+        const res = await cloudinary.v2.uploader.upload(dataURI);
+        return res.url;
+      });
+
+      const imageUrls = await Promise.all(uploadPromises);
+      newHotel.imageUrls = imageUrls;
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId;
+
+      const hotel = new Hotel(newHotel);
+      await hotel.save();
+
+      res.status(201).send(hotel);
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+export default router;
