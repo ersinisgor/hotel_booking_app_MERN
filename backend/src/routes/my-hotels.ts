d7d07cc4fb4f220d236e40c7bed5b4f1687ce658
Ersin Isgor <ersinisgor@hotmail.com>
2025-03-06 23:07:44 +0300

    feat_be: add validation for hotel creation fields and implement token verification

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import multer from "multer";
 import cloudinary from "cloudinary";
 import Hotel, { HotelType } from "../models/hotels";
+import verifyToken from "../middleware/auth";
+import { body } from "express-validator";
 
 const router = express.Router();
 
@@ -15,6 +17,22 @@ const upload = multer({
 
 router.post(
   "/",
+  verifyToken,
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("description").notEmpty().withMessage("Description is required"),
+    body("type").notEmpty().withMessage("Hotel type is required"),
+    body("pricePerNight")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Price per night is required and must be a number"),
+    body("facilities")
+      .notEmpty()
+      .isArray()
+      .withMessage("Facilities are required"),
+  ],
   upload.array("imageFiles", 6),
   async (req: Request, res: Response) => {
     try {
