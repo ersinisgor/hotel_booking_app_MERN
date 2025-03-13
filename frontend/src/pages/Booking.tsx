32d01faab4c56fdb01bcaeddac4c21a94e47783a
Ersin Isgor <ersinisgor@hotmail.com>
2025-03-13 20:38:12 +0300

    feat_fe: add BookingDetailsSummary component and integrate it into Booking page

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,18 +1,50 @@
 import { useQuery } from "@tanstack/react-query";
 import * as apiClient from "../api-client";
 import BookingForm from "../forms/BookingFrom/BookingForm";
+import { useParams } from "react-router-dom";
+import { useSearchContext } from "../contexts/SearchContext";
+import { useEffect, useState } from "react";
+import BookingDetailsSummary from "../components/BookingDetailsSummary";
 
 const Booking = () => {
+  const search = useSearchContext();
+  const { hotelId } = useParams();
+
+  const [numberOfNights, setNumberOfNights] = useState<number>(0);
+
+  useEffect(() => {
+    if (search.checkIn && search.checkOut) {
+      const nights =
+        Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) /
+        (1000 * 60 * 60 * 24);
+
+      setNumberOfNights(Math.ceil(nights));
+    }
+  }, [search.checkIn, search.checkOut]);
+
+  const { data: hotel } = useQuery({
+    queryKey: ["fetchHotelByID", hotelId],
+    queryFn: () => apiClient.fetchHotelById(hotelId as string),
+    enabled: !!hotelId,
+  });
+
   const { data: currentUser } = useQuery({
     queryKey: ["fetchCurrentUser"],
     queryFn: apiClient.fetchCurrentUser,
   });
 
-  console.log(currentUser?.email);
+  if (!hotel) return <></>;
 
   return (
     <div className="grid md:grid-cols-[1fr_2fr]">
-      <div className="bg-green-200">BOOKING DETAILS SUMMARY</div>
+      <BookingDetailsSummary
+        checkIn={search.checkIn}
+        checkOut={search.checkOut}
+        adultCount={search.adultCount}
+        childCount={search.childCount}
+        numberOfNights={numberOfNights}
+        hotel={hotel}
+      />
       {currentUser && <BookingForm currentUser={currentUser} />}
     </div>
   );
