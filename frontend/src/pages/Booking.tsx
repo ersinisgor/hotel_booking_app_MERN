68a11e79ff76a1941b52b385f6bac73e9b701fc9
Ersin Isgor <ersinisgor@hotmail.com>
2025-03-13 20:19:06 +0300

    feat_fe: implement BookingForm component and integrate it into Booking page

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import * as apiClient from "../api-client";
+import BookingForm from "../forms/BookingFrom/BookingForm";
 
 const Booking = () => {
   const { data: currentUser } = useQuery({
@@ -9,7 +10,12 @@ const Booking = () => {
 
   console.log(currentUser?.email);
 
-  return <div></div>;
+  return (
+    <div className="grid md:grid-cols-[1fr_2fr]">
+      <div className="bg-green-200">BOOKING DETAILS SUMMARY</div>
+      {currentUser && <BookingForm currentUser={currentUser} />}
+    </div>
+  );
 };
 
 export default Booking;
