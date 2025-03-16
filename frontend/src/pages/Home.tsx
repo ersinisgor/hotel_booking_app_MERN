74e54b3edbc66b1c97487ea5f05fae02a898a469
Ersin Isgor <ersinisgor@hotmail.com>
2025-03-16 14:28:27 +0300

    feat_fe: create Home component to display latest destinations

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="space-y-3">
+      <h2 className="text-3xl font-bold">Latest Destinations</h2>
+      <p>Most recent desinations added by our hosts</p>
+      <div className="grid gap-4">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-4"></div>
+        <div className="grid md:grid-cols-3 gap-4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
